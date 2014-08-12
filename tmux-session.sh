8cd2d3c72616c0a07e5e3a6a3cb0c0b28def1844
Zsolt Fekete <zsoltf@me.com>
2014-08-11 20:15:34 -0700

    saves layouts

--- tmux-session.sh
+++ tmux-session.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#]!/bin/bash
 
 clear
 
@@ -17,48 +17,99 @@ pane_parameters() {
     fi
     echo -n "directory: "
     echo "$2 "
-    echo "$3 "
+    #echo "$3 "
     shift 3
   done
   #child=$(pgrep -P $p)
 }
 
 pane_test() {
-  echo $@
-  echo
-  echo $1 $2 $3 $4
-  echo
-  shift 4
+  #echo $@
+  #echo
+  echo $1 $2 $3 $4 $5
+  initial_window=true
+  initial_pane=true
+  session=$1
+  window_index=$2
+  layout=$5
+  shift 5
 
   while [ $# -gt 2 ] ; do
     child=$(pgrep -P $1)
-    echo
-    echo -n "command: "
     if [ -z $child ]
     then
-      ps -o 'args=' -p $1
+      command=$(ps -o 'args=' -p $1)
     else
-      ps -o 'args=' -p $child
+      command=$(ps -o 'args=' -p $child)
     fi
-    echo -n "directory: "
-    echo "$2 "
-    echo "$3 "
+    if [ "$command" == "-bash" ]; then
+      command="cd $2"
+    else
+      command="cd $2 && $command"
+    fi
+
+    if [ "$session" = "$last_session" ]; then
+      initial_window=false
+    fi
+    if [ "$window_index" = "$last_window" ]; then
+      initial_pane=false
+    fi
+
+#    echo
+#    echo '###'
+#    echo initial session: $initial_session
+#    echo window index: $window_index
+#    echo last window: $last_window
+#    echo initial window: $initial_window
+#    echo initial pane: $initial_pane
+#    echo '###'
+#    echo
+#
+    if [ "$initial_window" == "true" ] && [ "$initial_pane" = "true" ]; then
+      echo "tmux new-session -d -s $session"
+      initial_session=false
+    elif [ "$initial_window" == "true" ] || [ "$initial_pane" = "true" ]; then
+      echo "tmux new-window -d -t $session:$window_index"
+    else
+      echo "tmux split-window -d -t $session:$window_index"
+    fi
+    last_session=$session
+    last_window=$window_index
+    #echo tmux send-keys -t $session:$window_index \"$command\" C-m
+    #echo $layout
+    #echo $last_window
+    #echo $initial_pane
+    #echo "$3 "
+    echo tmux select-layout -t $last_session:$last_window \"$layout\" \> /dev/null
     shift 3
-  done
+  done >> ./test_session
 }
 
 window_parameters() {
   #tmux list-panes -t $1:$2
   session=$1
+  window_index=$2
   name=$3
   nr_of_panes=$4
   layout=$5
 
   panes=$(tmux list-panes -t $1:$2 -F "#{pane_pid} #{pane_current_path} #{pane_current_command}")
-  pane_test $session $nr_of_panes $name $layout $panes
+  pane_test $session $window_index $nr_of_panes $name $layout $panes
 }
 
-windows=$(tmux list-windows -F "#{session_name} #{window_index} #{window_name} #{window_panes} #{window_layout}")
+# clear test session file before launch
+> ./test_session
+
+#echo tmux start-server >> ./test_session
+
+if $(tmux has-session)
+then
+  echo tmux is running.
+  echo Saving session and attaching...
+  #tmux -2u att
+fi
+
+windows=$(tmux list-windows -a -F "#{session_name} #{window_index} #{window_name} #{window_panes} #{window_layout}")
 
 while read window
 do
@@ -66,3 +117,5 @@ do
   window_parameters $window
   echo
 done <<< "$windows"
+
+cat ./test_session
