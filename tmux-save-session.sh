a8cc96a39f73cd976a4ab21189f6aa02e2f1e099
Zsolt Fekete <zsoltf@me.com>
2016-09-09 11:42:14 -0700

    open splits in the foreground for sendkeys. Fixes #3

--- tmux-save-session.sh
+++ tmux-save-session.sh
@@ -41,12 +41,13 @@ construct_panes() {
       echo "tmux new-session -d -n $window_name -s $session -c "$2""
       initial_session=false
     elif [ "$initial_window" == "true" ] || [ "$initial_pane" = "true" ]; then
-      echo "tmux new-window -d -n $window_name -t $session:$window_index -c "$2""
+      echo "tmux new-window -n $window_name -t $session:$window_index -c "$2""
     else
-      echo "tmux split-window -d -t $session:$window_index -c "$2""
+      echo "tmux split-window -t $session:$window_index -c "$2""
     fi
     # $3 - pane index
-    [ "$command" ] && echo tmux send-keys -t $session:$window_index.$3 \"$command\" C-m
+    echo "sleep 0.1"
+    [ "$command" ] && echo tmux send-keys -t $session:$window_index.$3 \"$command\" Enter
     echo tmux select-layout -t $session:$window_index \"$layout\" \> /dev/null
     last_session=$session
     last_window=$window_index
